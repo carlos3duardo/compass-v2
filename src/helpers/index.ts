d6c03bd3b70b31bf399c2b00bbb047141942c475
Carlos Eduardo <carloseduardo@cenarioweb.com.br>
2025-06-16 14:39:11 -0300

    feat: :sparkles: adicionando helpers ao projeto
    Adicionado funções para manipulação de string, mascaras e datas.

---
+++ src/helpers/index.ts
@@ -0,0 +1,30 @@
+import { currency, dateBirthBr, dateBr, dateTimeBr } from './locale';
+import {
+  maskCartaoCredito,
+  maskCep,
+  maskCnpj,
+  maskCpf,
+  maskPrimeiroPagamento,
+  maskTelefone,
+} from './masks';
+import { capitalize, firstName, initials, shortName } from './string';
+
+export {
+  // strings
+  capitalize,
+  currency,
+  dateBirthBr,
+  // locale
+  dateBr,
+  dateTimeBr,
+  firstName,
+  initials,
+  // masks
+  maskCartaoCredito,
+  maskCep,
+  maskCnpj,
+  maskCpf,
+  maskPrimeiroPagamento,
+  maskTelefone,
+  shortName,
+};
